6a7113a8b89d0cad7d267265741b6990f124b68a
F1NTURA <v33622@gmail.com>
2025-02-08 14:41:15 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,55 @@
+let i = 1;
+
+for (;;){
+        console.log("Privet");
+        i++;
+}
+
+let a = 1;
+
+for (;;){
+        console.log("Privet");
+        a++;
+}
+
+let b = 1;
+
+for (;;){
+        console.log("Privet");
+        b++;
+}
+
+let H = 1;
+
+for (;;){
+        console.log("Privet");
+        H++;
+}
+
+let J = 1;
+
+for (;;){
+        console.log("Privet");
+        J++;
+}
+
+let C = 1;
+
+for (;;){
+        console.log("Privet");
+        C++;
+}
+
+let Z = 1;
+
+for (;;){
+        console.log("Privet");
+        Z++;
+}
+
+let W = 1;
+
+for (;;){
+        console.log("Privet");
+        W++;
+}
